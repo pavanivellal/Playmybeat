b46c187fb3adecdda402c160878c27f3d6ef25d3
pavanivellal <pavani.vellal@sjsu.edu>
2016-04-16 00:25:24 -0700

    Additional Features

---
+++ app/src/main/java/com/example/pavanivellal/playmybeat/Add_Songs.java
@@ -0,0 +1,16 @@
+package com.example.pavanivellal.playmybeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_Songs extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__songs);
+
+    }
+}
